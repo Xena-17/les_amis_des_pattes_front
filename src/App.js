c227488ebd17609af710d0b2e664e742dea2891f
Josiane <josiane.bureau17@gmail.com>
2024-09-16 17:06:13 +0200

    intégration des routes du router sur App.js

--- src/App.js
+++ src/App.js
@@ -5,8 +5,13 @@ import footer from './Components/footer';
 import accueil from './Pages/accueil';
 import connexion from './Pages/connexion';
 import contact from './Pages/contact';
-import reservation from './Pages/reservation';
 import creation_compte from './Pages/creation_compte';
+import recherche from './Pages/recherche';
+import paiement from './Pages/paiement';
+import premiere_connexion from './Pages/premiere_connexion';
+import reservation from './Pages/reservation';
+import parametres_compte from './Pages/parametres_compte';
+
 
 
 import './App.css'; 
@@ -17,15 +22,17 @@ function App() {
       <div id="root">
         <header />
         <main>
-          <Routes>       
-             
-            <Route path="/" element={<accueil />} />
-            <Route path="/connexion" element={<connexion />} />
-            <Route path="/contact" element={<contact/>} />
-            <Route path="/users/:role" element={<connexion />} />
+          <Routes>
 
-            <Route path="/reservation/:id" element={<reservation />} />
-          
+            <Route path="/" element={<accueil />} />
+            <Route path="/users/:role" element={<connexion/>} />
+            <Route path="/contact" element={<contact/>} />
+            <Route path="/creation_compte" element={<creation_compte/>} />
+            <Route path="/paiement" element={<paiement />} />
+            <Route path="/parametres_compte" element={<parametres_compte />} />
+            <Route path="/premiere_connexion" element={<premiere_connexion />} />
+            <Route path="/recherche" element={<contact />} />
+            <Route path="/reservation/:id" element={<reservation />} />         
 
           </Routes>
         </main>
